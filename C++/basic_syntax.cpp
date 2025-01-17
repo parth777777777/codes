de9cd7745ffd669e0ca5c6531dc545154a86bf68
parth777777777 <parthbotalji@gmail.com>
2025-01-17 13:07:10 +0530

    yes

---
+++ C++/basic_syntax.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int length = 10;
+	float meow = 0.1;
+	double woof = 0.0230020230230;
+	char code = 'a';
+	string str = "it is very meow today";
+	cout << str<<endl;
+}
